f1ee2c7e3ea24d2d44c683fdb81b5a409f08458d
Turbobot <turbobot@vercel.com>
2024-05-23 12:22:38 -0300

    feat(create-turbo): apply official-starter transform

--- apps/commune-page/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@repo/ui/button";
-
-export default function Page(): JSX.Element {
-  return (
-    <main>
-      <Button appName="docs">Click me!</Button>
-    </main>
-  );
-}
