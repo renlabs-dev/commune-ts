65b9793bdfa9c4635c15db7e87a7226751a50072
Ed Castro <ed.sdr@outlook.com>
2024-05-23 14:48:58 -0300

    feat: fucking tailwind config

---
+++ packages/ui/src/data.ts
@@ -0,0 +1,12 @@
+export const links = {
+  docs: "/docs/getting-started/intro",
+  github: "https://github.com/commune-ai",
+  discord: "https://discord.com/invite/A8JGkZ9Dmm",
+  x: "https://twitter.com/communeaidotorg",
+  telegram: "https://t.me/communecommunity",
+  blog: "https://mirror.xyz/0xD80E194aBe2d8084fAecCFfd72877e63F5822Fc5",
+
+  wallet: "https://wallet.communeai.org/",
+  landing_page: "https://communeai.org/",
+  governance: "https://governance.communeai.org/",
+};
