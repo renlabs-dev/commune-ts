e5c4896961bc2e8db3f0c88063d057e84f67316a
Ed Castro <ed.sdr@outlook.com>
2024-05-23 16:57:07 -0300

    feat: basic header structure

--- apps/commune-page/src/app/layout.tsx
+++ apps/commune-page/src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import "@repo/ui/styles.css";
 import type { Metadata } from "next";
 import { cairo } from "@repo/ui/fonts";
+import { Header } from "@repo/ui/header";
+import { links } from "@repo/ui/data";
 
 export const metadata: Metadata = {
   robots: "all",
@@ -17,7 +19,19 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={cairo.className}>{children}</body>
+      <body className={`bg-green-950 ${cairo.className}`}>
+        <Header
+          logoSrc="/logo.svg"
+          navigationLinks={[
+            { name: "Governance", href: links.governance, external: true },
+            { name: "Docs", href: links.docs, external: false },
+            { name: "Blog", href: links.blog, external: true },
+            { name: "Join Community", href: links.discord, external: true },
+          ]}
+          title="Commune AI"
+        />
+        {children}
+      </body>
     </html>
   );
 }
