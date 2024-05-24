7dd3e06fa099f40ecd063e69d24407cd758c9a67
Ed Castro <ed.sdr@outlook.com>
2024-05-24 17:59:13 -0300

    chore(commune-page): trigger for deploy test

--- apps/commune-page/src/app/page.tsx
+++ apps/commune-page/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page(): JSX.Element {
   return (
     <main>
       <Test />
-      <div className="font-bold">yo</div>
+      <div className="font-bold">y</div>
     </main>
   );
 }
