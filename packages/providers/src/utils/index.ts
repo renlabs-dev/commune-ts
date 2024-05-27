28f63b79a54cb6d2fddd87bc2dcaff8c7c63002e
Ed Castro <ed.sdr@outlook.com>
2024-05-27 16:17:07 -0300

    feat: wallet tx methods and querys

---
+++ packages/providers/src/utils/index.ts
@@ -0,0 +1,7 @@
+export function calculateAmount(amount: string): number {
+  return Math.floor(Number(amount) * 10 ** 9);
+}
+
+export function smallAddress(address: string): string {
+  return `${address.slice(0, 8)}…${address.slice(-8)}`;
+}
