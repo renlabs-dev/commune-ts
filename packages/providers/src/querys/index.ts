e70c9d7d64473b69f146bc97ace0a1257a5618dd
Ed Castro <ed.sdr@outlook.com>
2024-05-27 21:23:13 -0300

    feat(providers): rpc querys for stake data

--- packages/providers/src/querys/index.ts
+++ packages/providers/src/querys/index.ts
@@ -1,7 +1,7 @@
 import "@polkadot/api-augment";
 import type { ApiPromise } from "@polkadot/api";
 import { formatToken, parseDaos } from "../utils";
-import { type DaoApplications } from "../types";
+import { StakeData, type DaoApplications } from "../types";
 
 // == Balance ==
 
@@ -45,4 +45,59 @@ export async function getDaoApplications(
 export async function getGlobalDaoTreasury(api: ApiPromise): Promise<string> {
   const result = await api.query.subspaceModule.globalDaoTreasury();
   return formatToken(Number(JSON.stringify(result)));
+}
+
+// == RPC ==
+
+export async function lastBlock(api: ApiPromise) {
+  const blockHeader = await api.rpc.chain.getHeader();
+  const blockNumber = blockHeader.number.toNumber();
+  const blockHash = blockHeader.hash;
+  const blockHashHex = blockHash.toHex();
+  const apiAtBlock = await api.at(blockHeader.hash);
+  return {
+    blockHeader,
+    blockNumber,
+    blockHash,
+    blockHashHex,
+    apiAtBlock,
+  };
+}
+
+export async function getAllStakeOut(api: ApiPromise) {
+  const { apiAtBlock, blockNumber, blockHashHex } = await lastBlock(api);
+  const stakeToQuery = await apiAtBlock.query.subspaceModule.stakeTo.entries();
+
+  let total = 0n;
+  const perAddr = new Map<string, bigint>();
+  const perNet = new Map<number, bigint>();
+  const perAddrPerNet = new Map<number, Map<string, bigint>>();
+
+  for (const [keyRaw, valueRaw] of stakeToQuery) {
+    const [netuidRaw, fromAddrRaw] = keyRaw.args;
+    const netuid = netuidRaw.toPrimitive() as number;
+    const fromAddr = fromAddrRaw.toString();
+    const stakeToMapForKey = valueRaw.toJSON() as Record<
+      string,
+      string | number
+    >;
+
+    for (const moduleKey in stakeToMapForKey) {
+      const staked = BigInt(stakeToMapForKey[moduleKey]);
+
+      total += staked;
+      perAddr.set(fromAddr, (perAddr.get(fromAddr) ?? 0n) + staked);
+      perNet.set(netuid, (perNet.get(netuid) ?? 0n) + staked);
+
+      const mapNet = perAddrPerNet.get(netuid) ?? new Map<string, bigint>();
+      mapNet.set(fromAddr, (mapNet.get(fromAddr) ?? 0n) + staked);
+      perAddrPerNet.set(netuid, mapNet);
+    }
+  }
+
+  return {
+    blockNumber,
+    blockHashHex,
+    stakeOut: { total, perAddr, perNet, perAddrPerNet },
+  } as StakeData;
 }
