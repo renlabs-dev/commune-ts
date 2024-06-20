edfc96f963ff56019de7731c604b0cc954b9586d
Ed Castro <ed.sdr@outlook.com>
2024-06-20 13:39:49 -0300

    feat: initial community validator app config

---
+++ apps/commune-validator/src/utils/applications-list.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import { links } from "@repo/ui/data";
+
+export const applicationsList = [
+  {
+    title: "Governance",
+    description: "Community Proposals",
+    href: links.governance,
+    icon: (
+      <Image
+        alt="Governance icon"
+        className="mb-5 w-12"
+        height={45}
+        src="/governance-icon.svg"
+        width={45}
+      />
+    ),
+  },
+  {
+    title: "Docs",
+    description: "Dev Documentation",
+    href: links.docs,
+    icon: (
+      <Image
+        alt="Docs icon"
+        className="mb-5 w-12"
+        height={45}
+        src="/docs-icon.svg"
+        width={45}
+      />
+    ),
+  },
+  {
+    title: "Blog",
+    target: "_blank",
+    description: "View recent updates",
+    href: links.blog,
+    icon: (
+      <Image
+        alt="Updates icon"
+        className="mb-5 w-12"
+        height={45}
+        src="/updates-icon.svg"
+        width={45}
+      />
+    ),
+  },
+  {
+    title: "Join Community",
+    description: null,
+    href: links.discord,
+    icon: (
+      <Image
+        alt="Community icon"
+        className="mb-5 w-12"
+        height={45}
+        src="/community-icon.svg"
+        width={45}
+      />
+    ),
+  },
+];
