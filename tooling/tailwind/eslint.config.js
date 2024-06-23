78560c1dacb675a5710bb18f81e7eb8ccb80cccb
Ed Castro <ed.sdr@outlook.com>
2024-06-23 10:17:28 -0300

    chore: new packages organization and config

---
+++ tooling/tailwind/eslint.config.js
@@ -0,0 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../tooling/eslint/types.d.ts" />
+
+import baseConfig from "@commune-ts/eslint-config/base";
+
+export default [...baseConfig];
