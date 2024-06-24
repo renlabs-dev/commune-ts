33c5593de7bec1e5f59831ff4054db7a393e43e8
Ed Castro <ed.sdr@outlook.com>
2024-06-24 10:44:59 -0300

    feat: add `db` and `api` packages

---
+++ packages/api/src/trpc.ts
@@ -0,0 +1,81 @@
+/**
+ * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
+ * 1. You want to modify request context (see Part 1)
+ * 2. You want to create a new middleware or type of procedure (see Part 3)
+ *
+ * tl;dr - this is where all the tRPC server stuff is created and plugged in.
+ * The pieces you will need to use are documented accordingly near the end
+ */
+import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
+import { ZodError } from "zod";
+
+import { db } from "@commune-ts/db/client";
+
+/**
+ * 1. CONTEXT
+ *
+ * This section defines the "contexts" that are available in the backend API.
+ *
+ * These allow you to access things when processing a request, like the database, the session, etc.
+ *
+ * This helper generates the "internals" for a tRPC context. The API handler and RSC clients each
+ * wrap this and provides the required context.
+ *
+ * @see https://trpc.io/docs/server/context
+ */
+export const createTRPCContext = (opts: {
+  headers: Headers;
+  session: null;
+}) => {
+  const source = opts.headers.get("x-trpc-source") ?? "unknown";
+  console.log(">>> tRPC Request from", source);
+
+  return {
+    db,
+  };
+};
+
+/**
+ * 2. INITIALIZATION
+ *
+ * This is where the trpc api is initialized, connecting the context and
+ * transformer
+ */
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter: ({ shape, error }) => ({
+    ...shape,
+    data: {
+      ...shape.data,
+      zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+    },
+  }),
+});
+
+/**
+ * Create a server-side caller
+ * @see https://trpc.io/docs/server/server-side-calls
+ */
+export const createCallerFactory = t.createCallerFactory;
+
+/**
+ * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
+ *
+ * These are the pieces you use to build your tRPC API. You should import these
+ * a lot in the /src/server/api/routers folder
+ */
+
+/**
+ * This is how you create new routers and sub routers in your tRPC API
+ * @see https://trpc.io/docs/router
+ */
+export const createTRPCRouter = t.router;
+
+/**
+ * Public procedure
+ *
+ * This is the base piece you use to build new queries and mutations on your
+ * tRPC API.
+ */
+export const publicProcedure = t.procedure;
---
+++ packages/db/src/schema.ts
@@ -0,0 +1,62 @@
+import { sql } from "drizzle-orm";
+import {
+  boolean,
+  integer,
+  pgTableCreator,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+const ss58Address = (name: string) => varchar(name, { length: 256 });
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = pgTableCreator((name) => `${name}`);
+
+/**
+ * A module on the Commune network considered by our system.
+ */
+export const moduleItem = createTable("moduleItem", {
+  address: ss58Address("address").primaryKey(),
+  lastSeenBlock: integer("last_seen_block"), // updated_at kinda
+  metadataUri: text("metadata_uri"),
+  // if uid is null (worker will set) the module is de-registered
+  uid: varchar("uid", { length: 256 }),
+  // other module data UwU
+});
+
+/**
+ * Data for the relation a user have with a specific module.
+ * The user can set a weight (vote) for a module, and favorite it.
+ */
+export const userModuleData = createTable("user_module_data", {
+  userAddress: ss58Address("user_address").primaryKey(),
+  moduleAddress: ss58Address("module_address").references(
+    () => moduleItem.address,
+  ),
+  weight: integer("weight").default(0),
+  favorite: boolean("favorite").default(false),
+});
+
+export const moduleReport = createTable("module_report", {
+  id: serial("id").primaryKey(),
+  userAddress: ss58Address("address"),
+  moduleAddress: ss58Address("address").references(() => moduleItem.address),
+  content: text("content"),
+  reason: varchar("reason", { length: 16 }),
+});
+
+// TODO:
+export const moduleWeight = createTable("module_weigh", {
+  id: serial("id").primaryKey(),
+  weight: integer("weight").default(0),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+});
