87bad08f1ec5845ca873c636b57d0e3e4f00a672
Ed Castro <ed.sdr@outlook.com>
2024-06-24 15:47:13 -0300

    feat: update database provider to postgres

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,6 +1,17 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: postgres.Sql | undefined;
+};
+
+const conn = globalForDb.conn ?? postgres(String(process.env.POSTGRES_URL));
+// if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+export const db = drizzle(conn, { schema });
