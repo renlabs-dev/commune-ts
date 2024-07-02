a5d9f26deb149c99bea1d08c6bfadb13ff4b3f7b
Ed Castro <ed.sdr@outlook.com>
2024-07-02 20:06:35 -0300

    feat(db, api): config polish for requests

--- apps/commune-validator/src/app/page.tsx
+++ apps/commune-validator/src/app/page.tsx
@@ -1,3 +1,19 @@
+import { api } from "~/trpc/server";
+
 export default function Page(): JSX.Element {
-  return <div>uwu</div>;
+  return <CrudShowcase />;
+}
+
+async function CrudShowcase() {
+  const latestModule = await api.moduleTest.getLatest();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestModule ? (
+        <p className="truncate">Most recent module id: {latestModule.id}</p>
+      ) : (
+        <p>No modules found</p>
+      )}
+    </div>
+  );
 }
