96716a6276ed58e63c1141af6d7e762ead390411
Ed Castro <ed.sdr@outlook.com>
2024-07-05 18:33:13 -0300

    feat: polish on stepper component

--- apps/communex-roadmap/src/app/page.tsx
+++ apps/communex-roadmap/src/app/page.tsx
@@ -1,51 +1,12 @@
 "use client";
 
-import React from "react";
-
+import { roadmapData } from "~/roadmap-data";
 import Stepper from "./components/Stepper";
 
-const steps = {
-  "Q3 2024": [
-    {
-      title: "Step 1",
-      description: "Step 1 description",
-    },
-    {
-      title: "Step 2",
-      description: "Step 2 description",
-    },
-    {
-      title: "Step 3",
-      description: "Step 3 description",
-    },
-    {
-      title: "Step 4",
-      description: "Step 4 description",
-    },
-    {
-      title: "Step 5",
-      description: "Step 5 description",
-    },
-  ],
-  "Q4 2024": [
-    {
-      title: "X 1",
-      description: "Step 1 description",
-    },
-    {
-      title: "Y 2",
-      description: "Step 2 description",
-    },
-    {
-      title: "Z 3",
-      description: "Step 3 description",
-    },
-  ],
-};
 export default function HomePage(): JSX.Element {
   return (
-    <div className="App">
-      <Stepper steps={steps} />
+    <div className="mx-auto max-w-screen-2xl px-4">
+      <Stepper steps={roadmapData} />
     </div>
   );
 }
