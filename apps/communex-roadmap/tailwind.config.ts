51340ab4b0d829354bb7bf0ac8af21dd26592b89
Ed Castro <ed.sdr@outlook.com>
2024-07-05 18:33:13 -0300

    feat: polish on stepper component

--- apps/communex-roadmap/tailwind.config.ts
+++ apps/communex-roadmap/tailwind.config.ts
@@ -7,5 +7,12 @@ export default {
   // those classes are included correctly.
   content: [...baseConfig.content, "../../packages/ui/**/*.{ts,tsx}"],
   presets: [baseConfig],
-  theme: {},
+  theme: {
+    extend: {},
+    boxShadow: {
+      "custom-green": "0px 0px 12px 0 rgba(34, 197, 94, 0.90)",
+      "custom-white": "0px 0px 12px 0 rgba(255, 255, 255, 0.90)",
+      "custom-gray": "0px 0px 12px 0 rgba(156, 163, 175, 0.90)",
+    },
+  },
 } satisfies Config;
