c16d801bf91857fd038af5edf08e9e2badc9651c
Ed Castro <ed.sdr@outlook.com>
2024-07-07 21:58:57 -0300

    feat: add delegate module percentage state functionality

--- apps/commune-validator/src/app/layout.tsx
+++ apps/commune-validator/src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
             wallet={<WalletButtonWithHook />}
           />
           <TRPCReactProvider>{children}</TRPCReactProvider>
-          <Footer />
+          <Footer shouldBeFixed />
         </Providers>
       </body>
     </html>
