180a21216c53e19556824f79e13fa2e39f1ab279
Ed Castro <ed.sdr@outlook.com>
2024-07-07 21:58:57 -0300

    feat: add delegate module percentage state functionality

---
+++ apps/commune-validator/src/app/components/copy-square-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Square2StackIcon } from "@heroicons/react/24/outline";
+
+import { toast } from "@commune-ts/providers/use-toast";
+import { copyToClipboard } from "@commune-ts/providers/utils";
+
+interface CopySquareButtonProps {
+  address: string;
+}
+
+export function CopySquareButton(props: CopySquareButtonProps) {
+  function handleCopy() {
+    copyToClipboard(props.address);
+    toast.success("Copied to clipboard");
+  }
+  return (
+    <button
+      className="border border-white/20 bg-[#898989]/5 p-2 backdrop-blur-md transition duration-200 hover:border-green-500 hover:bg-green-500/10"
+      onClick={handleCopy}
+    >
+      <Square2StackIcon className="h-6 w-6 text-gray-400 transition duration-200 hover:text-green-500" />
+    </button>
+  );
+}
--- apps/commune-validator/src/app/components/delegate-module-percentage.tsx
+++ apps/commune-validator/src/app/components/delegate-module-percentage.tsx
@@ -1,16 +1,40 @@
+"use client";
+
+import { useState } from "react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
 
-// TODO: Delegation logic
+// TODO: Delegation submit logic
 export function DelegateModulePercentage() {
+  const [percentage, setPercentage] = useState(0);
+
+  function handlePercentageChange(change: number) {
+    setPercentage((prevPercentage) => {
+      const newPercentage = prevPercentage + change;
+      if (newPercentage > 100) return 100;
+      if (newPercentage < 0) return 0;
+      return newPercentage;
+    });
+  }
+
   return (
-    <div className="flex items-center gap-2 border border-white/20 bg-[#898989]/5 px-2 backdrop-blur-md">
-      <button>
-        <ChevronDownIcon className="h-5 w-5 text-red-500" />
-      </button>
-      <div className="flex items-center justify-center border-x border-white/20 px-2 py-2">
-        100%
+    <>
+      <div className="flex w-full items-center gap-2 border border-white/20 bg-[#898989]/5 backdrop-blur-md">
+        <button
+          className="border-r border-white/20 p-2 hover:bg-red-500/10"
+          onClick={() => handlePercentageChange(-10)}
+        >
+          <ChevronDownIcon className="h-5 w-5 text-red-500" />
+        </button>
+        <div className="flex w-full items-center justify-center border-white/20 p-2">
+          {percentage}%
+        </div>
+        <button
+          className="border-l border-white/20 p-2 hover:bg-green-500/10"
+          onClick={() => handlePercentageChange(10)}
+        >
+          <ChevronUpIcon className="h-5 w-5 text-green-500" />
+        </button>
       </div>
-      <ChevronUpIcon className="h-5 w-5 text-green-500" />
-    </div>
+    </>
   );
 }
