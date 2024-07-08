62751e97bcb814b1119708a6ea68d5ad7a87932b
Ed Castro <ed.sdr@outlook.com>
2024-07-07 21:59:09 -0300

    feat: update ModuleCard component to fetch and display custom metadata

--- apps/commune-validator/src/app/components/module-card.tsx
+++ apps/commune-validator/src/app/components/module-card.tsx
@@ -1,34 +1,46 @@
 import Link from "next/link";
-import {
-  ArrowRightIcon,
-  Square2StackIcon,
-  Squares2X2Icon,
-} from "@heroicons/react/24/outline";
+import { ArrowRightIcon, Squares2X2Icon } from "@heroicons/react/24/outline";
 
+import type { CustomMetadata } from "@commune-ts/providers/types";
+import { fetchCustomMetadata } from "@commune-ts/providers/hooks";
 import { smallAddress } from "@commune-ts/providers/utils";
 
+import { CopySquareButton } from "./copy-square-button";
 import { DelegateModulePercentage } from "./delegate-module-percentage";
 
 interface ModuleCardProps {
   id: number;
-  title: string; // IPFS
-  description: string; // IPFS
   address: string; // SS58
+  metadata: string | null; // IPFS
 }
 
-export function ModuleCard(props: ModuleCardProps) {
+interface CustomMetadata {
+  Ok: {
+    title: string;
+    body: string;
+  };
+}
+
+export async function ModuleCard(props: ModuleCardProps) {
+  const metadata = (await fetchCustomMetadata(
+    "proposal",
+    props.id,
+    props.metadata ?? "",
+  )) as CustomMetadata;
+
+  const title = metadata.Ok.title;
+  const description = metadata.Ok.body;
+
   return (
-    <div className="flex flex-col gap-2 border border-white/20 bg-[#898989]/5 p-6 text-gray-400 backdrop-blur-md">
-      <h2 className="text-xl font-semibold text-white">{props.title}</h2>
-      <p className="mb-1">{props.description}</p>
+    <div className="flex w-full flex-col gap-2 border border-white/20 bg-[#898989]/5 p-6 text-gray-400 backdrop-blur-md">
+      <h2 className="text-xl font-semibold text-white">{title}</h2>
+      <p className="mb-1">{description}</p>
       <div className="flex items-center justify-between gap-2">
         <span className="flex w-full items-center gap-2 border border-white/20 bg-[#898989]/5 p-2 backdrop-blur-md">
           <Squares2X2Icon className="h-6 w-6 text-green-500" />{" "}
           {smallAddress(String(props.address))}
         </span>
-        <button className="border border-white/20 bg-[#898989]/5 p-2 backdrop-blur-md transition duration-200 hover:border-green-500 hover:bg-green-500/10">
-          <Square2StackIcon className="h-6 w-6 text-gray-400 transition duration-200 hover:text-green-500" />
-        </button>
+        <CopySquareButton address={props.address} />
       </div>
       <div className="flex items-center justify-between gap-2">
         <DelegateModulePercentage />
