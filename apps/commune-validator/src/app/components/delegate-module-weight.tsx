f073e23dd1212def91cfd70eec193f1a5ed0a2b6
Ed Castro <ed.sdr@outlook.com>
2024-07-08 22:02:21 -0300

    feat: update delegateModuleWeight component

--- apps/commune-validator/src/app/components/delegate-module-weight.tsx
+++ apps/commune-validator/src/app/components/delegate-module-weight.tsx
@@ -3,17 +3,29 @@
 import { useState } from "react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
 
-// TODO: Delegation submit logic
-export function DelegateModulePercentage() {
-  const [percentage, setPercentage] = useState(0);
+type Change = "up" | "down";
 
-  function handlePercentageChange(change: number) {
-    setPercentage((prevPercentage) => {
-      const newPercentage = prevPercentage + change;
-      if (newPercentage > 100) return 100;
-      if (newPercentage < 0) return 0;
-      return newPercentage;
-    });
+// TODO: Delegation submit logic
+export function DelegateModuleWeight() {
+  const [weight, setWeight] = useState<number | null>(null);
+
+  const BASE_WEIGHT = 100;
+
+  function handleWeightChange(change: Change) {
+    if (weight === null) {
+      if (change === "up") {
+        setWeight(BASE_WEIGHT);
+      } else {
+        setWeight(0);
+      }
+      return;
+    }
+
+    if (change === "up") {
+      setWeight(weight + 10);
+    } else {
+      setWeight(Math.max(weight - 10, 0));
+    }
   }
 
   return (
@@ -21,16 +33,16 @@ export function DelegateModulePercentage() {
       <div className="flex w-full items-center gap-2 border border-white/20 bg-[#898989]/5 backdrop-blur-md">
         <button
           className="border-r border-white/20 p-2 hover:bg-red-500/10"
-          onClick={() => handlePercentageChange(-10)}
+          onClick={() => handleWeightChange("down")}
         >
           <ChevronDownIcon className="h-5 w-5 text-red-500" />
         </button>
         <div className="flex w-full items-center justify-center border-white/20 p-2">
-          {percentage}%
+          {weight}
         </div>
         <button
           className="border-l border-white/20 p-2 hover:bg-green-500/10"
-          onClick={() => handlePercentageChange(10)}
+          onClick={() => handleWeightChange("up")}
         >
           <ChevronUpIcon className="h-5 w-5 text-green-500" />
         </button>
