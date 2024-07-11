cefd025e532357a109339f68b045cb9c39763b18
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-07-11 18:42:48 -0300

    fix: inverted names on button and new button scan

--- packages/ui/src/data.ts
+++ packages/ui/src/data.ts
@@ -1,13 +1,15 @@
 export const links = {
-  docs: "/docs/getting-started/intro",
-  github: "https://github.com/commune-ai",
-  discord: "https://discord.com/invite/A8JGkZ9Dmm",
-  x: "https://twitter.com/communeaidotorg",
-  telegram: "https://t.me/communecommunity",
-  blog: "https://mirror.xyz/0xD80E194aBe2d8084fAecCFfd72877e63F5822Fc5",
-
-  wallet: "https://wallet.communeai.org/",
-  landing_page: "https://communeai.org/",
   about: "/about",
+  docs: "/docs/getting-started/intro",
+
+  blog: "https://mirror.xyz/0xD80E194aBe2d8084fAecCFfd72877e63F5822Fc5",
+  discord: "https://discord.com/invite/A8JGkZ9Dmm",
+  github: "https://github.com/commune-ai",
+  telegram: "https://t.me/communecommunity",
+  x: "https://twitter.com/communeaidotorg",
+
+  explorer: "https://stats.communex.ai/",
   governance: "https://governance.communeai.org/",
+  landing_page: "https://communeai.org/",
+  wallet: "https://wallet.communeai.org/",
 };
