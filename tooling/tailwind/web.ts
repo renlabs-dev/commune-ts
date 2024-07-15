c11d60b6d557a908f92898b275634f25ec918e8c
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-07-15 17:12:53 -0300

    push missed tooling changes

--- tooling/tailwind/web.ts
+++ tooling/tailwind/web.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animated";
+import typography from '@tailwindcss/typography'
 
 import base from "./base";
 
@@ -29,5 +30,8 @@ export default {
       },
     },
   },
-  plugins: [animate],
+  plugins: [
+    animate,
+    typography
+  ],
 } satisfies Config;
