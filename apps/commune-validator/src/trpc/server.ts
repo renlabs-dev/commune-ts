93ce1f815312003e71755dc888adaf8f8a89c9f8
Ed Castro <ed.sdr@outlook.com>
2024-07-19 11:30:06 -0300

    feat: Add ModuleViewControl component for module page

--- apps/commune-validator/src/trpc/server.ts
+++ apps/commune-validator/src/trpc/server.ts
@@ -7,7 +7,7 @@ import { createCaller, createTRPCContext } from "@commune-ts/api";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(async () => {
+const createContext = cache(() => {
   const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
 
