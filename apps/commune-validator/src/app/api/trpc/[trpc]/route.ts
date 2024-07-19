9ecee7d5fe0726d6954464e2a2f9e6bc66441a69
Ed Castro <ed.sdr@outlook.com>
2024-07-19 16:24:37 -0300

    feat: Update runtime to "edge" in trpc route and module page

--- apps/commune-validator/src/app/api/trpc/[trpc]/route.ts
+++ apps/commune-validator/src/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,8 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter, createTRPCContext } from "@commune-ts/api";
 
+export const runtime = "edge";
+
 /**
  * Configure basic CORS headers
  */
