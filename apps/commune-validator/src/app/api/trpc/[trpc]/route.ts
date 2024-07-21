1f14109c1383e6685dc9f69e645ec3b47c34fa59
Ed Castro <ed.sdr@outlook.com>
2024-07-21 10:00:29 -0300

    feat: Update pagination controls styling for module page, revert runtime edge for local build

--- apps/commune-validator/src/app/api/trpc/[trpc]/route.ts
+++ apps/commune-validator/src/app/api/trpc/[trpc]/route.ts
@@ -2,7 +2,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
 import { appRouter, createTRPCContext } from "@commune-ts/api";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 /**
  * Configure basic CORS headers
