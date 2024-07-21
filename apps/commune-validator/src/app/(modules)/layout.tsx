fa9b45b1e52312c00bf859904988112014910cbc
Ed Castro <ed.sdr@outlook.com>
2024-07-21 10:00:29 -0300

    feat: Update pagination controls styling for module page, revert runtime edge for local build

--- apps/commune-validator/src/app/(modules)/layout.tsx
+++ apps/commune-validator/src/app/(modules)/layout.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
+  const pathname = usePathname();
   return (
     <main className="mx-auto flex w-full max-w-screen-2xl flex-col items-start justify-center px-4 text-white">
       <div className="my-20 flex w-full flex-col gap-3 pb-4">
@@ -19,13 +23,13 @@ export default function RootLayout({
       <div className="mb-4 flex w-full animate-fade-down flex-col gap-4 border-b border-white/20 pb-4 animate-delay-300 md:flex-row">
         <Link
           href="/"
-          className="w-full gap-2 border border-white/20 bg-[#898989]/5 p-3 text-center text-lg text-white transition duration-200 hover:border-green-500 hover:bg-green-500/10"
+          className={`w-full gap-2 border  bg-[#898989]/5 p-3 text-center text-lg text-white transition duration-200 hover:border-green-500 hover:bg-green-500/10 ${pathname === "/" ? "border-green-500 bg-green-500/10" : "border-white/20"}`}
         >
           All
         </Link>
         <Link
           href="/weighted"
-          className="w-full gap-2 border border-white/20 bg-[#898989]/5 p-3 text-center text-lg text-white transition duration-200 hover:border-green-500 hover:bg-green-500/10"
+          className={`w-full gap-2 border bg-[#898989]/5 p-3 text-center text-lg text-white transition duration-200 hover:border-green-500 hover:bg-green-500/10 ${pathname === "/weighted" ? "border-green-500 bg-green-500/10" : "border-white/20"}`}
         >
           Weighted
         </Link>
