5880116ae8248b2133fc4049b7e81039527b01cc
Gabriel Cipriano <gabriel@cipriano.dev>
2024-08-04 18:22:29 +0200

    feat: proposal comment routes

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
 import { moduleRouter } from "./router/module";
+import { proposalCommentRouter } from "./router/proposal-comment";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   module: moduleRouter,
+  proposalComment: proposalCommentRouter,
 });
 
 // export type definition of API
