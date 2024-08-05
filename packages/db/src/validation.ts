b138e6df539064b64656bd28df696608a50ebd32
Ed Castro <ed.sdr@outlook.com>
2024-08-05 09:25:57 -0300

    fix: format

--- packages/db/src/validation.ts
+++ packages/db/src/validation.ts
@@ -1,32 +1,58 @@
 import { createInsertSchema } from "drizzle-zod";
-import { commentInteractionSchema, proposalCommentSchema, VoteType, commentReportSchema, ReportReason} from "./schema";
 import { z } from "zod";
 
-export const PROPOSAL_COMMENT_INSERT_SCHEMA = createInsertSchema(proposalCommentSchema, {
-  content: z.string().min(1).max(512).transform((v) => v.trim()).refine(s => s.length, "Comment should not be blank"),
-  userKey: z.string().min(1),
-  proposalId: z.number().int(),
-}).omit({
+import {
+  commentInteractionSchema,
+  commentReportSchema,
+  proposalCommentSchema,
+  ReportReason,
+  VoteType,
+} from "./schema";
+
+export const PROPOSAL_COMMENT_INSERT_SCHEMA = createInsertSchema(
+  proposalCommentSchema,
+  {
+    content: z
+      .string()
+      .min(1)
+      .max(512)
+      .transform((v) => v.trim())
+      .refine((s) => s.length, "Comment should not be blank"),
+    userKey: z.string().min(1),
+    proposalId: z.number().int(),
+  },
+).omit({
   id: true,
   createdAt: true,
   deletedAt: true,
 });
 
-export const COMMENT_INTERACTION_INSERT_SCHEMA = createInsertSchema(commentInteractionSchema, {
-  commentId: z.string(),
-  userKey: z.string(),
-  voteType: z.nativeEnum(VoteType),
-}).omit({
+export const COMMENT_INTERACTION_INSERT_SCHEMA = createInsertSchema(
+  commentInteractionSchema,
+  {
+    commentId: z.string(),
+    userKey: z.string(),
+    voteType: z.nativeEnum(VoteType),
+  },
+).omit({
   id: true,
   createdAt: true,
 });
 
-export const COMMENT_REPORT_INSERT_SCHEMA = createInsertSchema(commentReportSchema, {
-  commentId: z.string(),
-  userKey: z.string(),
-  reason: z.nativeEnum(ReportReason),
-  content: z.string().min(1).max(512).transform((v) => v.trim()).refine(s => s.length, "Comment should not be blank"),
-}).omit({
+export const COMMENT_REPORT_INSERT_SCHEMA = createInsertSchema(
+  commentReportSchema,
+  {
+    commentId: z.string(),
+    userKey: z.string(),
+    reason: z.nativeEnum(ReportReason),
+    content: z
+      .string()
+      .min(1)
+      .max(512)
+      .transform((v) => v.trim())
+      .refine((s) => s.length, "Comment should not be blank"),
+  },
+).omit({
   id: true,
   createdAt: true,
 });
