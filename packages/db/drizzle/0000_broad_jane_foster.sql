cfa29af8fa3b059dcddda13a5d7a594abd50e67f
Ed Castro <ed.sdr@outlook.com>
2024-08-06 15:57:44 -0300

    feat: basic proposal discussion structure

---
+++ packages/db/drizzle/0000_broad_jane_foster.sql
@@ -0,0 +1,24 @@
+-- Custom SQL migration file, put you code below! --
+CREATE VIEW comment_digest AS
+SELECT
+  pc.id,
+  pc.proposal_id AS "proposalId",
+  pc.user_key AS "userKey",
+  pc.content,
+  pc.created_at AS "createdAt",
+  COALESCE(SUM(CASE WHEN ci.vote_type = 'UP' THEN 1 ELSE 0 END), 0) AS upvotes,
+  COALESCE(SUM(CASE WHEN ci.vote_type = 'DOWN' THEN 1 ELSE 0 END), 0) AS downvotes
+FROM
+  proposal_comment pc
+LEFT JOIN
+  comment_interaction ci ON pc.id = ci.comment_id
+WHERE
+  pc.deleted_at IS NULL
+GROUP BY
+  pc.id,
+  pc.proposal_id,
+  pc.user_key,
+  pc.content,
+  pc.created_at
+ORDER BY
+  pc.created_at ASC;
