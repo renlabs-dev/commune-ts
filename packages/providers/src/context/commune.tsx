32c20348871fdca6f7801e0282438cb113bf30b5
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-08-12 16:25:06 -0300

    fix: wip

--- packages/providers/src/context/commune.tsx
+++ packages/providers/src/context/commune.tsx
@@ -142,7 +142,7 @@ export function CommuneProvider({
       web3Accounts,
       web3FromAddress,
     });
-    const provider = new WsProvider('wss://commune.api.onfinality.io/public-ws');
+    const provider = new WsProvider(wsEndpoint);
     const newApi = await ApiPromise.create({ provider });
     setApi(newApi);
     setIsInitialized(true);
