005a6cfab2ab3a959ecad7222a710c6fbcf7ef87
Ed Castro <ed.sdr@outlook.com>
2024-08-13 13:30:55 -0300

    feat: fix loader on home

--- apps/commune-governance/src/app/components/balance-section.tsx
+++ apps/commune-governance/src/app/components/balance-section.tsx
@@ -30,6 +30,10 @@ export function BalanceSection({
     userStakeWeight = userStakeEntry ?? 0n;
   }
 
+  const balanceLoaded = typeof balance !== "undefined";
+
+  console.log(balanceLoaded);
+
   return (
     <div
       className={`flex w-full flex-col items-center justify-center lg:mt-10 ${className ?? ""}`}
@@ -37,7 +41,7 @@ export function BalanceSection({
       <div
         className={`flex w-full flex-col divide-gray-500 border-white/20 text-2xl text-green-500 lg:flex-row lg:gap-6 lg:pb-5`}
       >
-        <div className="flex animate-fade-down flex-row items-center justify-between border-white/20 bg-[#898989]/5 p-6 pr-6 backdrop-blur-md lg:w-1/3 lg:border lg:pr-10">
+        <div className="flex animate-fade-down flex-row items-center justify-between border-white/20 bg-[#898989]/5 p-6 pr-6 lg:w-1/3 lg:border lg:pr-10">
           <div className="flex flex-col gap-1">
             {!daoTreasury && !isInitialized ? (
               <p className="animate-pulse text-gray-400">
@@ -57,8 +61,22 @@ export function BalanceSection({
           <Image alt="Dao Icon" height={40} src="/dao-icon.svg" width={40} />
         </div>
 
-        <div className="flex animate-fade-down flex-row items-center justify-between border-t !border-white/20 bg-[#898989]/5 p-6 pr-6 backdrop-blur-md animate-delay-100 lg:w-1/3 lg:border lg:pr-10">
+        <div className="flex animate-fade-down flex-row items-center justify-between border-t !border-white/20 bg-[#898989]/5 p-6 pr-6 animate-delay-100 lg:w-1/3 lg:border lg:pr-10">
           <div className="flex flex-col items-start gap-1">
+            {!isInitialized && (
+              <p className="animate-pulse text-gray-400">
+                Loading...
+                <span className="text-lg text-white"> COMAI</span>
+              </p>
+            )}
+
+            {isInitialized && !balanceLoaded && (
+              <p className="animate-pulse text-gray-400">
+                Loading...
+                <span className="text-lg text-white"> COMAI</span>
+              </p>
+            )}
+
             {isInitialized && !selectedAccount?.meta.name && (
               <button
                 className="inline-flex items-center justify-center text-gray-300 hover:text-green-600"
@@ -71,18 +89,13 @@ export function BalanceSection({
             )}
 
             {isInitialized &&
-            selectedAccount?.meta.name &&
-            typeof balance !== "undefined" ? (
-              <p>
-                {formatToken(balance)}
-                <span className="text-lg text-white"> COMAI</span>
-              </p>
-            ) : (
-              <p className="text-gray-400">
-                Loading...
-                <span className="text-lg text-white"> COMAI</span>
-              </p>
-            )}
+              selectedAccount?.meta.name &&
+              typeof balance !== "undefined" && (
+                <p>
+                  {formatToken(balance)}
+                  <span className="text-lg text-white"> COMAI</span>
+                </p>
+              )}
 
             <span className="text-base font-light text-gray-200">
               Your total free balance
@@ -96,7 +109,7 @@ export function BalanceSection({
           />
         </div>
 
-        <div className="flex animate-fade-down flex-row items-center justify-between border-t !border-white/20 bg-[#898989]/5 p-6 pr-6 backdrop-blur-md animate-delay-200 lg:w-1/3 lg:border lg:pr-10">
+        <div className="flex animate-fade-down flex-row items-center justify-between border-t !border-white/20 bg-[#898989]/5 p-6 pr-6 animate-delay-200 lg:w-1/3 lg:border lg:pr-10">
           <div className="flex flex-col items-start gap-1">
             {!isInitialized ||
             (selectedAccount?.meta.name && userStakeWeight == null) ? (
