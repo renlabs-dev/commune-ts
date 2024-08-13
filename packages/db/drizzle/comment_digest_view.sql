d6fa7d09ed857a83aca130bdae8f678110dd0b66
Ed Castro <ed.sdr@outlook.com>
2024-08-13 19:26:14 -0300

    feat: db structure for user name on comments

---
+++ packages/db/drizzle/comment_digest_view.sql
@@ -0,0 +1,41 @@
+-- Custom SQL migration file, put you code below! --
+CREATE VIEW
+  comment_digest AS
+SELECT
+  pc.id,
+  pc.proposal_id,
+  pc.user_key,
+  pc.user_name,
+  pc.content,
+  pc.created_at,
+  COALESCE(
+    SUM(
+      CASE
+        WHEN ci.vote_type = 'UP' THEN 1
+        ELSE 0
+      END
+    ),
+    0
+  ) AS upvotes,
+  COALESCE(
+    SUM(
+      CASE
+        WHEN ci.vote_type = 'DOWN' THEN 1
+        ELSE 0
+      END
+    ),
+    0
+  ) AS downvotes
+FROM
+  proposal_comment pc
+  LEFT JOIN comment_interaction ci ON pc.id = ci.comment_id
+WHERE
+  pc.deleted_at IS NULL
+GROUP BY
+  pc.id,
+  pc.proposal_id,
+  pc.user_key,
+  pc.content,
+  pc.created_at
+ORDER BY
+  pc.created_at ASC;
