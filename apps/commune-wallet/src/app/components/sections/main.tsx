d6cbceba4e03735307d7f8cf3bb503ac1feed3bc
Ed Castro <ed.sdr@outlook.com>
2024-08-19 18:30:12 -0300

    feat: wallet front end structural refactor

--- apps/commune-wallet/src/app/components/sections/main.tsx
+++ apps/commune-wallet/src/app/components/sections/main.tsx
@@ -3,13 +3,45 @@
 import { useCommune } from "@commune-ts/providers/use-commune";
 
 import { IntroSection } from "./intro";
-import { WalletSection } from "./wallet";
+import { Wallet } from "./wallet";
 
 export function MainSection() {
-  const { selectedAccount } = useCommune();
+  const {
+    // Wallet
+    balance,
+    stakeOut,
+    // Connections
+    handleConnect,
+    selectedAccount,
+    // Transactions
+    addStake,
+    removeStake,
+    transfer,
+    transferStake,
+  } = useCommune();
   return (
     <main className="flex min-h-[86dvh] flex-col items-center justify-center gap-3 text-white">
-      {selectedAccount ? <WalletSection /> : <IntroSection />}
+      {selectedAccount ? (
+        <Wallet.Root>
+          <Wallet.Header
+            handleConnect={handleConnect}
+            selectedAccount={selectedAccount}
+          />
+          <Wallet.Balance
+            balance={balance ? balance : 0n}
+            stakeOut={stakeOut}
+            selectedAccount={selectedAccount}
+          />
+          <Wallet.Actions
+            addStake={addStake}
+            removeStake={removeStake}
+            transfer={transfer}
+            transferStake={transferStake}
+          />
+        </Wallet.Root>
+      ) : (
+        <IntroSection />
+      )}
     </main>
   );
 }
