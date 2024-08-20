90e2ff16015741088b94ec18eab776d3fd55bea6
Ed Castro <ed.sdr@outlook.com>
2024-08-19 22:28:25 -0300

    feat: buttons with correct color variants

--- apps/commune-wallet/src/app/components/icon-button.tsx
+++ apps/commune-wallet/src/app/components/icon-button.tsx
@@ -1,25 +1,28 @@
 "use client";
 
-import type { MenuType } from "~/utils/types";
+import type { ColorType, MenuType } from "~/utils/types";
 import { Icon } from "./icon";
 
 interface IconButtonProps {
   src: string;
   text: string;
-  color: string;
+  color: ColorType;
   activeMenu: MenuType;
   setActiveMenu: (menu: MenuType) => void;
 }
 
 export function IconButton(props: IconButtonProps) {
+  const colorVariants = {
+    red: `text-red-500 hover:bg-red-500/15 active:bg-red-500/40 border border-red-500/50 hover:border-red-500/70 ${props.activeMenu === props.text ? "bg-red-500/20" : "bg-red-500/5"}`,
+    amber: `text-amber-500 hover:bg-amber-500/15 active:bg-amber-500/40 border border-amber-500/50 hover:border-amber-500/70 ${props.activeMenu === props.text ? "bg-amber-500/20" : "bg-amber-500/5"}`,
+    purple: `text-purple-500 hover:bg-purple-500/15 active:bg-purple-500/40 border border-purple-500/50 hover:border-purple-500/70 ${props.activeMenu === props.text ? "bg-purple-500/20" : "bg-purple-500/5"}`,
+    green: `text-green-500 hover:bg-green-500/15 active:bg-green-500/40 border border-green-500/50 hover:border-green-500/70 ${props.activeMenu === props.text ? "bg-green-500/20" : "bg-green-500/5"}`,
+  };
+
   return (
     <button
-      onClick={() => props.setActiveMenu(props.text.toLowerCase() as MenuType)}
-      className={`flex w-full items-center justify-center gap-3 text-nowrap border border-white/20 px-6 py-2.5 font-semibold transition duration-200 hover:bg-[#898989]/10 text-${props.color}-500 ${
-        props.activeMenu === props.text.toLowerCase()
-          ? `bg-${props.color}-500/20`
-          : "bg-[#898989]/5"
-      }`}
+      onClick={() => props.setActiveMenu(props.text as MenuType)}
+      className={`flex w-full items-center justify-center gap-3 text-nowrap border border-white/20 px-6 py-2.5 font-semibold transition duration-200 hover:bg-[#898989]/10 ${colorVariants[props.color]}`}
     >
       <Icon src={props.src} className="h-5 w-5 md:h-6 md:w-6" />
       <p className="text-lg">{props.text}</p>
