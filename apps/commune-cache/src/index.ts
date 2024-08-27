75b04904fd65c3df193c0e3602defa706720cd8b
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-08-27 10:22:09 -0300

    fix: tsconfig and pipeline errors

--- apps/commune-cache/src/index.ts
+++ apps/commune-cache/src/index.ts
@@ -185,7 +185,7 @@ app.get("/api/health/details", (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
