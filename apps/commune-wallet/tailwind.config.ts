64f1b3169e8cb9d97f6746c42a509bec0798b876
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-08-28 15:53:58 -0300

    fix: tailwind config to force new deploy

--- apps/commune-wallet/tailwind.config.ts
+++ apps/commune-wallet/tailwind.config.ts
@@ -6,7 +6,10 @@ import baseConfig from "@commune-ts/tailwind-config/web";
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
-  content: [...baseConfig.content, "../../packages/ui/**/*.{ts,tsx}"],
+  content: [
+    ...baseConfig.content,
+    "../../packages/ui/src/components/*.{ts,tsx}",
+  ],
   presets: [baseConfig, typography],
   theme: {},
 } satisfies Config;
