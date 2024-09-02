a6f640ecbed804e550d1f537c9fb593eccdacdba
Ed Castro <ed.sdr@outlook.com>
2024-09-02 12:33:13 -0300

    feat: Define new DB tables and structure for adding and validating DAO Members
    Co-authored-by: PsicoThePato <PsicoThePato@users.noreply.github.com>

--- apps/comrads-worker/src/index.ts
+++ apps/comrads-worker/src/index.ts
@@ -3,17 +3,18 @@ import "@polkadot/api-augment";
 import { WsProvider } from "@polkadot/api";
 import express from "express";
 
-import type { SQL, Table } from "@commune-ts/db";
-import type { LastBlock, SubspaceModule } from "@commune-ts/types";
-import { getTableColumns, sql } from "@commune-ts/db";
-import { db } from "@commune-ts/db/client";
-import { moduleData } from "@commune-ts/db/schema";
+import type { LastBlock } from "@commune-ts/types";
 import {
   ApiPromise,
   queryLastBlock,
   queryRegisteredModulesInfo,
 } from "@commune-ts/subspace/queries";
 
+import { computeTotalVotesPerDao, upsertModuleData } from "./db";
+
+type WorkerType = "dao" | "validator";
+const workerName = process.argv[2] as WorkerType;
+const blockTime = 8000;
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -37,100 +38,105 @@ async function setup(): Promise<ApiPromise> {
   return api;
 }
 
-async function main() {
-  const api = await setup();
+interface WorkerProps {
+  lastBlockNumber: number;
+  lastBlock: LastBlock;
+  api: ApiPromise;
+}
 
-  let lastBlockNumber = -1;
-  let lastBlock: LastBlock;
-
+function is_new_block(saved_block: number, queried_block: number) {
+  if (saved_block === queried_block) {
+    log(`Block ${queried_block} already processed, skipping`);
+    return false;
+  }
+  return true;
+}
+async function run_validator_worker(props: WorkerProps) {
   // eslint-disable-next-line no-constant-condition
   while (true) {
     try {
       const currentTime = new Date();
-      lastBlock = await queryLastBlock(api);
-      if (lastBlock.blockNumber === lastBlockNumber) {
-        log(`Block ${lastBlock.blockNumber} already processed, skipping`);
-        await sleep(1000);
+      const lastBlock = await queryLastBlock(props.api);
+      if (!is_new_block(props.lastBlock.blockNumber, lastBlock.blockNumber)) {
+        await sleep(blockTime);
         continue;
       }
-      lastBlockNumber = lastBlock.blockNumber;
+      props.lastBlock = lastBlock;
 
-      log(`Block ${lastBlock.blockNumber}: processing`);
+      log(`Block ${props.lastBlock.blockNumber}: processing`);
 
       const modules = await queryRegisteredModulesInfo(
-        lastBlock.apiAtBlock,
+        props.lastBlock.apiAtBlock,
         ["name", "address", "metadata", "registrationBlock"],
         [NETUID_ZERO],
       );
 
       log(
-        `Block ${lastBlock.blockNumber}: upserting  ${modules.length} modules`,
+        `Block ${props.lastBlock.blockNumber}: upserting  ${modules.length} modules`,
       );
 
-      await upsertModuleData(modules, lastBlock.blockNumber);
+      await upsertModuleData(modules, props.lastBlock.blockNumber);
 
       log(
-        `Block ${lastBlock.blockNumber}: module data upserted in ${(new Date().getTime() - currentTime.getTime()) / 1000} seconds`,
+        `Block ${props.lastBlock.blockNumber}: module data upserted in ${(new Date().getTime() - currentTime.getTime()) / 1000} seconds`,
       );
     } catch (e) {
       log("UNEXPECTED ERROR: ", e);
-      await sleep(1000);
+      await sleep(blockTime);
       continue;
     }
   }
 }
 
-async function upsertModuleData(modules: SubspaceModule[], atBlock: number) {
-  await db
-    .insert(moduleData)
-    .values(
-      modules.map((m) => ({
-        netuid: m.netuid,
-        moduleKey: m.key,
-        name: m.name,
-        atBlock,
-        addressUri: m.address,
-        metadataUri: m.metadata,
-        registrationBlock: m.registrationBlock,
-      })),
-    )
-    .onConflictDoUpdate({
-      target: [moduleData.netuid, moduleData.moduleKey],
-      set: buildConflictUpdateColumns(moduleData, [
-        "atBlock",
-        "addressUri",
-        "metadataUri",
-        "registrationBlock",
-        "name",
-      ]),
-    })
-    .execute();
+async function run_dao_worker(props: WorkerProps) {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    try {
+      const lastBlock = await queryLastBlock(props.api);
+      if (!is_new_block(props.lastBlock.blockNumber, lastBlock.blockNumber)) {
+        await sleep(blockTime);
+        continue;
+      }
+      props.lastBlock = lastBlock;
+
+      log(`Block ${props.lastBlock.blockNumber}: processing`);
+      const votes = await computeTotalVotesPerDao();
+      console.log(votes);
+    } catch (e) {
+      log("UNEXPECTED ERROR: ", e);
+      await sleep(blockTime);
+      continue;
+    }
+  }
 }
 
-// util for upsert https://orm.drizzle.team/learn/guides/upsert#postgresql-and-sqlite
-function buildConflictUpdateColumns<
-  T extends Table,
-  Q extends keyof T["_"]["columns"],
->(table: T, columns: Q[]): Record<Q, SQL> {
-  const cls = getTableColumns(table);
-  return columns.reduce(
-    (acc, column) => {
-      const colName = cls[column]?.name;
-      acc[column] = sql.raw(`excluded.${colName}`);
-      return acc;
-    },
-    {} as Record<Q, SQL>,
-  );
+async function main(worker_type: WorkerType) {
+  const api = await setup();
+  const lastBlockNumber = -1;
+  const lastBlock = await queryLastBlock(api);
+  if (worker_type === "validator") {
+    await run_validator_worker({
+      lastBlock,
+      api,
+      lastBlockNumber,
+    });
+  } else {
+    await run_dao_worker({
+      lastBlock,
+      api,
+      lastBlockNumber,
+    });
+  }
 }
 
-main()
+main(workerName)
   .catch(console.error)
   .finally(() => process.exit());
 
 const app = express();
 
 app.get("/api/health", (_, res) => {
-  res.send("OK");
+  res.send(`${workerName} OK`);
 });
 
 const port = process.env.PORT ?? 3000;
