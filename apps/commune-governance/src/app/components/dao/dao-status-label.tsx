2eaad0e74a77ccb1e633d239699f92f7380529b0
Ed Castro <ed.sdr@outlook.com>
2024-09-05 17:32:21 -0300

    feat: working type refactor
    Co-authored-by: PsicoThePato <PsicoThePato@users.noreply.github.com>

--- apps/commune-governance/src/app/components/dao/dao-status-label.tsx
+++ apps/commune-governance/src/app/components/dao/dao-status-label.tsx
@@ -10,22 +10,22 @@ export const DaoStatusLabel = (props: StatusLabelProps): JSX.Element => {
   const { status } = props;
   const votingStatus = {
     Pending: (
-      <Label className="w-auto border border-yellow-500 bg-yellow-500/10 py-1.5 text-center text-yellow-500 lg:text-left ">
+      <Label className="w-auto border border-yellow-500 bg-yellow-500/10 py-1.5 text-center text-yellow-500 lg:text-left">
         Active
       </Label>
     ),
     Accepted: (
-      <Label className="w-auto border border-green-500 bg-green-500/10 py-1.5 text-center text-green-500 lg:text-left ">
+      <Label className="w-auto border border-green-500 bg-green-500/10 py-1.5 text-center text-green-500 lg:text-left">
         Accepted
       </Label>
     ),
     Refused: (
-      <Label className="w-auto border border-red-500 bg-red-500/10 py-1.5 text-center text-red-500 lg:text-left ">
+      <Label className="w-auto border border-red-500 bg-red-500/10 py-1.5 text-center text-red-500 lg:text-left">
         Refused
       </Label>
     ),
     Removed: (
-      <Label className="w-auto border border-rose-500 bg-rose-500/10 py-1.5 text-center text-rose-500 lg:text-left ">
+      <Label className="w-auto border border-rose-500 bg-rose-500/10 py-1.5 text-center text-rose-500 lg:text-left">
         Removed
       </Label>
     ),
