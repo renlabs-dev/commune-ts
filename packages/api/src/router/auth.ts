2e56404aa17fd1d0f5f47d8d06960c09bfe5ec35
Vinicius Porto do Sacramento <viniciusonps@gmail.com>
2024-09-17 16:50:55 -0300

    fix: format

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -8,7 +8,7 @@ import { publicProcedure } from "../trpc";
 export const authRouter = {
   startSession: publicProcedure
     .input(SignedPayloadSchema)
-    .mutation( async ({ input }) => {
+    .mutation(async ({ input }) => {
       const token = await createSessionToken(input);
       // TODO: somehow check that the userKey is an valid commune/substrate address
 
