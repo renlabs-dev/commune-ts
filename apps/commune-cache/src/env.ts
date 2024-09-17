5de21731f7079bd5758cf4a626b2f49893412f22
Ed Castro <ed.sdr@outlook.com>
2024-09-17 15:17:20 -0300

    feat: add `@t3-oss/env-core` structure to commune-cache

---
+++ apps/commune-cache/src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  shared: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+  },
+  clientPrefix: "NEXT_PUBLIC_",
+  client: {
+    NEXT_PUBLIC_PORT: z.number().default(3000),
+    NEXT_PUBLIC_WS_PROVIDER_URL: z.string().url(),
+  },
+  runtimeEnv: process.env,
+});
