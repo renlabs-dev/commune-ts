bdfecafd5dfb94f72d3598efb03a25bac7c08fc6
DaviPtrs <davispetris@gmail.com>
2024-09-19 11:45:18 -0300

    Revert "feat: add `t3-oss/env-core` to db package"
    This reverts commit 75b616460080824dce6cb426ea6f72266a427b38.

--- packages/db/drizzle.config.js
+++ packages/db/drizzle.config.js
@@ -1,6 +1,8 @@
-import { env } from "./env";
+if (!process.env.POSTGRES_URL) {
+  throw new Error("Missing POSTGRES_URL");
+}
 
-const nonPoolingUrl = env.POSTGRES_URL.replace(":6543", ":5432");
+const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
 
 export default {
   schema: "./src/schema.ts",
