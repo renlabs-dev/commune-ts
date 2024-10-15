d7386978801b03369af311a9e2a47384886a1d36
Ed Castro <ed.sdr@outlook.com>
2024-10-15 13:22:57 -0300

    feat: update delegated list component

--- apps/commune-validator/src/app/components/charts/subnet-pie-chart.tsx
+++ apps/commune-validator/src/app/components/charts/subnet-pie-chart.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ArrowTrendingUpIcon } from "@heroicons/react/16/solid";
-import { Label, Pie, PieChart, Cell } from "recharts";
+import { Cell, Label, Pie, PieChart } from "recharts";
 
 import type { ChartConfig } from "@commune-ts/ui";
 import {
@@ -18,32 +18,14 @@ import {
 } from "@commune-ts/ui";
 
 export const description = "A donut chart with text";
-const generateColors = (numColors: number) => {
-  const hueRanges = [
-    { start: 0, end: 60 }, // Red to Yellow
-    { start: 60, end: 120 }, // Yellow to Green
-    { start: 120, end: 180 }, // Green to Cyan
-    { start: 180, end: 240 }, // Cyan to Blue
-    { start: 240, end: 300 }, // Blue to Magenta
-    { start: 300, end: 360 }, // Magenta to Red
-  ];
 
-  const colors = [];
-
-  for (let i = 0; i < numColors; i++) {
-    const hueRange = hueRanges[i % hueRanges.length];
-    const hue = Math.floor(
-      hueRange.start + ((i / hueRanges.length) % 1) * (hueRange.end - hueRange.start)
-    );
-    const saturation = 70 + Math.random() * 30; // Random saturation between 70% and 100%
-    const lightness = 50 + Math.random() * 20; // Random lightness between 50% and 70%
-    const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-    colors.push(color);
-  }
-
-  return colors;
-};
-
+const chartColors = [
+  "hsl(var(--chart-secondary-1))",
+  "hsl(var(--chart-secondary-2))",
+  "hsl(var(--chart-secondary-3))",
+  "hsl(var(--chart-secondary-4))",
+  "hsl(var(--chart-secondary-5))",
+];
 
 interface SubnetData {
   subnetName: string;
@@ -54,6 +36,7 @@ interface SubnetData {
 interface SubnetPieChartProps {
   chartData: SubnetData[];
 }
+
 const chartConfig = {
   stakeWeight: {
     label: "stakeWeight",
@@ -62,31 +45,15 @@ const chartConfig = {
     label: "Chrome",
     color: "hsl(var(--chart-1))",
   },
-  safari: {
-    label: "Safari",
-    color: "hsl(var(--chart-2))",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
-  },
 } satisfies ChartConfig;
 
 export function SubnetPieChart({ chartData }: SubnetPieChartProps) {
   const totalStakeWeight = 100;
-  const colors = React.useMemo(() => generateColors(50), []);
-  // const colors = generateColors(50);
+
   const getFillColor = (index: number) => {
-    return colors[index % colors.length];
+    return chartColors[index % chartColors.length];
   };
+
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
@@ -150,11 +117,11 @@ export function SubnetPieChart({ chartData }: SubnetPieChartProps) {
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
         <div className="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month{" "}
+          Other Subnets: 5.2%
           <ArrowTrendingUpIcon className="h-4 w-4" />
         </div>
         <div className="leading-none text-muted-foreground">
-          Showing total stakeWeight for the last 6 months
+          Showing total stake allocation for the last block
         </div>
       </CardFooter>
     </Card>
