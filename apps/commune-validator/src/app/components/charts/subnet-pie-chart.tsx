216260a57ec62620a82a6b7ebfb14fc4d117f0c5
Ed Castro <ed.sdr@outlook.com>
2024-10-16 18:41:05 -0300

    fix: graphs
    Co-authored-by: PsicoThePato <PsicoThePato@users.noreply.github.com>

--- apps/commune-validator/src/app/components/charts/subnet-pie-chart.tsx
+++ apps/commune-validator/src/app/components/charts/subnet-pie-chart.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import type { TooltipProps } from "recharts";
 import * as React from "react";
-import { Cell, Pie, PieChart } from "recharts";
+import { Cell, Label, Pie, PieChart } from "recharts";
 
 import type { ChartConfig } from "@commune-ts/ui";
 import {
@@ -12,7 +13,6 @@ import {
   CardTitle,
   ChartContainer,
   ChartTooltip,
-  ChartTooltipContent,
 } from "@commune-ts/ui";
 
 export const description = "A donut chart with text";
@@ -46,29 +46,42 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export function SubnetPieChart({ chartData }: SubnetPieChartProps) {
-  const getFillColor = (index: number) => {
-    return chartColors[index % chartColors.length];
-  };
+const getFillColor = (index: number) => {
+  return chartColors[index % chartColors.length];
+};
 
+const CustomTooltipContent: React.FC<TooltipProps<number, string>> = ({
+  active,
+  payload,
+}) => {
+  if (active && payload?.length) {
+    const { subnetName, percWeight } = payload[0]?.payload as SubnetData;
+    const percentage = (percWeight * 100).toFixed(2);
+    return (
+      <Card className="flex justify-between gap-3 rounded-md p-2">
+        <p className="font-bold text-cyan-500">{subnetName}</p>
+        <p>{percentage}%</p>
+      </Card>
+    );
+  }
+  return null;
+};
+
+export function SubnetPieChart({ chartData }: SubnetPieChartProps) {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
         <CardTitle>Current Subnet Allocation</CardTitle>
         <CardDescription>
-          Showing total stakeWeight for the last 6 months
+          Showing aggregated subnet allocation from user inputs.
         </CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square max-h-[350px]"
+          className="mx-auto aspect-square max-h-[360px]"
         >
           <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
             <Pie
               data={chartData}
               dataKey="percWeight"
@@ -79,7 +92,37 @@ export function SubnetPieChart({ chartData }: SubnetPieChartProps) {
               {chartData.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={getFillColor(index)} />
               ))}
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {chartData.length.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy ?? 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Subnets
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
             </Pie>
+            <ChartTooltip cursor={false} content={<CustomTooltipContent />} />
           </PieChart>
         </ChartContainer>
       </CardContent>
